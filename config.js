8b1a17546c97014250f3e326cee1d4219744d36d
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-04-23 20:32:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const siteConfig = {
   siteName: "Doodle Jump 2",
-  siteURL: "https://doodlejump-2.github.io"
+  siteURL: "https://doodlejump-2.github.io",
+  footerColor: "#ff0000" // 🔴 Red color (you can change anytime)
 };
