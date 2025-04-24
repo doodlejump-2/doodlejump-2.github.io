e8980371dfa1903b4c5f6ee036290e6a5fbc08b8
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-04-24 17:50:46 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,14 @@
+// ads.js
+
+atOptions = {
+  'key' : '0dfb40ed676f77d4e4e7051bee1682e2',
+  'format' : 'iframe',
+  'height' : 90,
+  'width' : 728,
+  'params' : {}
+};
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//stemboastfulrattle.com/0dfb40ed676f77d4e4e7051bee1682e2/invoke.js';
+document.head.appendChild(script);
