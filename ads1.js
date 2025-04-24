16a85515aafc7bc66484ae0d9cc36a6be8636ffb
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-04-24 19:18:58 +0530

    Update ads1.js

--- ads1.js
+++ ads1.js
@@ -1,10 +1,1 @@
-window.addEventListener('DOMContentLoaded', function () {
-  var script = document.createElement('script');
-  script.src = 'https://funnygames.lol/ad.js';
-  script.async = true;
-
-  var container = document.getElementById('ad1-container');
-  if (container) {
-    container.appendChild(script);
-  }
-});
+document.write('<script src="https://funnygames.lol/ad.js"><\/script>');
