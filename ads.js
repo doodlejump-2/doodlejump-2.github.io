8718042f725b7b64400c422e46c53edf342ef0f0
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-05-03 20:10:17 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,18 +1,2 @@
-window.addEventListener('DOMContentLoaded', function () {
-  atOptions = {
-    'key' : '0dfb40ed676f77d4e4e7051bee1682e2',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
-  };
-
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = '//stemboastfulrattle.com/0dfb40ed676f77d4e4e7051bee1682e2/invoke.js';
-
-  var container = document.getElementById('ad-container');
-  if (container) {
-    container.appendChild(script);
-  }
-});
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7308059949367897"
+     crossorigin="anonymous"></script>
