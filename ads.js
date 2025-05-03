b738ba9e606de574fa892bc8f1af9df2ab0ac4b5
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-05-03 20:18:25 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,2 +1,9 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7308059949367897"
-     crossorigin="anonymous"></script>
+// This dynamically loads AdSense script
+(function() {
+  var adsScript = document.createElement('script');
+  adsScript.async = true;
+  adsScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7308059949367897";
+  adsScript.crossOrigin = "anonymous";
+  document.head.appendChild(adsScript);
+})();
+
