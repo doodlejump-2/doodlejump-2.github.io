5ebd1a7e25a46397b83ae607e8a13b44bb0cd0c5
linkflynetwork <161827312+linkflynetwork@users.noreply.github.com>
2025-05-20 20:20:44 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,9 +1,12 @@
-// This dynamically loads AdSense script
-(function() {
-  var adsScript = document.createElement('script');
-  adsScript.async = true;
-  adsScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7308059949367897";
-  adsScript.crossOrigin = "anonymous";
-  document.head.appendChild(adsScript);
-})();
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7308059949367897"
+     crossorigin="anonymous"></script>
+<!-- Doodle Jump -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7308059949367897"
+     data-ad-slot="5619609831"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
